e7d15536d3bce80116b7b7a444d52ec7cc0d8cb8
TobiPrae <tobias.praetori91@gmail.com>
2026-05-29 10:23:13 +0200

    Added expander functionality for "Management" pages

--- pages/Player_Management.py
+++ pages/Player_Management.py
@@ -13,10 +13,10 @@ if "success_msg" in st.session_state:
     st.success(st.session_state.success_msg)
     del st.session_state.success_msg
 
-st.subheader("Add player")
+st.subheader("Add Player")
 with st.form("add_player"):
     name = st.text_input("Player name", key="add_name")
-    submitted = st.form_submit_button("Add player")
+    submitted = st.form_submit_button("Add Player")
     if submitted:
         if not name:
             st.error("Player name is required")
@@ -26,29 +26,29 @@ with st.form("add_player"):
             st.rerun()
 
 
-st.subheader("Edit Players")
-players = client.list_players()
-players.sort(key=lambda x: x.player_name.lower())
+with st.expander("Edit Players", expanded=False):
+    players = client.list_players()
+    players.sort(key=lambda x: x.player_name.lower())
 
-if not players:
-    st.info("No players found. Add players above.")
-else:
-    selected_player = st.selectbox(
-        "Select a player to modify",
-        options=players,
-        format_func=lambda x: x.player_name
-    )
+    if not players:
+        st.info("No players found. Add players above.")
+    else:
+        selected_player = st.selectbox(
+            "Select a player to modify",
+            options=players,
+            format_func=lambda x: x.player_name
+        )
 
-    if selected_player:
-        with st.form(key=f"edit_player_form_{selected_player.id}"):
-            new_name = st.text_input("Player name", value=selected_player.player_name, key=f"new_name_{selected_player.id}")
-            save = st.form_submit_button("Save Changes")
-            #delete = st.form_submit_button("Delete Player")
-            
-            if save:
-                client.update_player(selected_player.id, player_name=new_name)
-                st.session_state.success_msg = "Player updated"
-                st.rerun()
+        if selected_player:
+            with st.form(key=f"edit_player_form_{selected_player.id}"):
+                new_name = st.text_input("Player name", value=selected_player.player_name, key=f"new_name_{selected_player.id}")
+                save = st.form_submit_button("Save Changes")
+                #delete = st.form_submit_button("Delete Player")
+                
+                if save:
+                    client.update_player(selected_player.id, player_name=new_name)
+                    st.session_state.success_msg = "Player updated"
+                    st.rerun()
             
             #if delete:
             #    confirm = st.checkbox("Confirm delete? This cannot be undone.", key=f"confirm_{selected_player.id}")
