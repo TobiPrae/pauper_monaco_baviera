65a519e2aa867154b05d551c9a4a1bf81b0fca03
TobiPrae <tobias.praetori91@gmail.com>
2026-06-02 15:38:11 +0200

    Clean Up unused files

--- tests/test_auth.py
+++
@@ -1,8 +0,0 @@
-from auth import hash_password, check_password
-
-
-def test_hash_and_check():
-    pwd = "s3cret"
-    h = hash_password(pwd)
-    assert check_password(pwd, h)
-    assert not check_password("wrong", h)
